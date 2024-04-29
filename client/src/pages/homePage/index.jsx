b2968f8fcfb79369632ab4ab77f1627fa1f41664
ajnaskp <ajnas@akiraplc.com>
2024-04-29 11:23:43 +0530

    client :navbar

--- client/src/pages/homePage/index.jsx
+++ client/src/pages/homePage/index.jsx
@@ -1,10 +1,15 @@
+import { Box } from '@mui/material'
+import LoginPage from '../loginPage/index.jsx'
+import Navbar from '../navbar/index.jsx'
 import React from 'react'
+
 
 const HomePage = () => {
   return (
-    <div>
-        Home page
-    </div>
+    <Box>
+      <Navbar/>
+
+    </Box>
   )
 }
 
